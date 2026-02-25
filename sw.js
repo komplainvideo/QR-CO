1ff50e38763642291bf58a01b7fae1f59996589e
komplainvideo <komplainvideo@gmail.com>
2026-02-25 19:46:40 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,17 @@
-const CACHE = 'qr-v10';
-const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+// SW yang menghapus dirinya sendiri dan semua cache
+self.addEventListener('install', function(e) {
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE).map(k => {
-        console.log('Deleting old cache:', k);
-        return caches.delete(k);
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => 
-      cached || fetch(e.request).catch(() => caches.match('./index.html'))
-    )
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      return self.clients.matchAll();
+    }).then(function(clients) {
+      clients.forEach(function(c) { c.navigate(c.url); });
+    })
   );
 });
